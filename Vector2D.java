d06b4969abc62668a9d9cc5c1b3e9aa11bc96b89
Sanchit <34150437+Sanchit170054@users.noreply.github.com>
2018-03-10 23:54:33 +0530

    Create Vector2D.java

---
+++ Vector2D.java
@@ -0,0 +1,24 @@
+package a_Introductory;
+
+public class Vector2D {
+	public Integer x, y;
+	
+	Vector2D(Integer x, Integer y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/* Construct Vector2D from two points */
+	Vector2D(Point p1, Point p2) {
+		this.x = p2.x - p1.x;
+		this.y = p2.y - p1.y;
+	}
+	
+	public int dotProduct(Vector2D v) {
+		return (x * v.x) + (y * v.x);
+	}
+	
+	public boolean isOrthogonalTo(Vector2D v) {
+		return (dotProduct(v) == 0); 
+	}
+}
