35f9be832bf7fab6a0e01c3db74ae65d695b74c3
Sanchit <34150437+Sanchit170054@users.noreply.github.com>
2018-03-10 23:56:15 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,11 @@
+package a_Introductory;
+
+public class Fibonacci {
+	public int fib(int n) {
+		switch (n) {
+			case 0: return 0;
+			case 1: return 1;
+			default: return (fib(n - 1) + fib(n - 2));
+		}
+	}
+} 
